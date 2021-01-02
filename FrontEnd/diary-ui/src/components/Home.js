86bedc340523425a15467e973a65bb196d93d6dc
Anjali2901 <LIT2016033@iiita.ac.in>
2021-01-02 22:50:56 +0530

    Added header component and home component Added background images for home page

---
+++ FrontEnd/diary-ui/src/components/Home.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Form from './common/Form';
+import styled from 'styled-components';
+import Login from './Login';
+import Signup from './Signup';
+import Header from './Header';
+
+function Home() {
+    
+    return (
+        <>
+
+        <Header />
+        {/* <Button>
+        <div>  <Login /> </div>
+        <div>  <Signup /> </div>
+        </Button> */} 
+           <div class="bg_image"
+        style={{
+          backgroundImage: 'url("./public/assets/images/UI_homepage_Cover.jpg")',
+          backgroundSize: "cover"
+        }}>
+        <Wrapper>
+           <h1>"Welcome to secret keeper of your profound thoughts and bright ideas."</h1>
+           <div>
+                "I thought about how there are two types of secrets: the kind you want to keep in, and the kind you don't dare to let out. - Ally Carter"
+            </div>
+            <div>
+                "A good friend keeps your secrets for you. A best friend helps you keep your own secrets. - Lauren Oliver " 
+            </div>     
+       </Wrapper>
+       </div> 
+       </>
+    );
+}
+
+export default Home;
+
+const Wrapper = styled.div`
+font-size: 2em;
+margin: 1em;
+padding: 1em;
+border-radius: 3px;
+boxShadow: 0px 0px 150px 10px;
+`; 
